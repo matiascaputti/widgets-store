09e637b8b610b8a80e2b174cc5ea50ac01f05c0f
maticaputti <matias.caputti@gmail.com>
2016-03-11 12:32:39 -0300

    Default props definitions

--- components/App.js
+++ components/App.js
@@ -4,13 +4,24 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render() {
         return <h1>
-            {this.props.txt}!</h1>
+            {this.props.txt}!
+            {this.props.num}!
+            </h1>
         // return React.createElement('h1', null, 'Hello!')
     }
 }
 
+App.propTypes = {
+    txt: React.PropTypes.string,
+    num: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: 'no text was passed'
+}
+
 ReactDOM.render(
-    <App txt="props text" />,
+    <App txt="props text" num={5} />,
     document.getElementById('app')
 );
 
