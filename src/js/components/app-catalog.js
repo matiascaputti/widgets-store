65dd0218415647b984492fc5248f316056476ae8
maticaputti <matias.caputti@gmail.com>
2016-03-14 15:13:17 -0300

    Add catalogItem component

--- src/js/components/app-catalog.js
+++ src/js/components/app-catalog.js
@@ -1,1 +1,27 @@
-app-catalog.js
+import React from 'react';
+import AppStore from '../stores/app-store';
+import CatalogItem from './app-catalogitem';
+
+function getCatalog() {
+    return { items: AppStore.getCatalog }
+}
+
+class Catalog extends React.Component {
+    constructor() {
+        super();
+        this.state = getCatalog();
+    }
+
+    renderer() {
+        let items = this.state.items.map( item => {
+            return <CatalogItem key={ item.id } item={ item } />
+        });
+        return (
+            <div classNN="row">
+                { items }
+            </div>
+        )
+    }
+}
+
+export default Catalog;
